718e827c76056411b34b7b057b3a9992e86c8621
Stefanos Zisidis <stefanos.zisidis@stcatherines.gr>
2026-09-17 10:36:33 +0300

    paper 2 review

---
+++ Yr11_STZ_coding_sessions/paper2_review1.py
@@ -0,0 +1,25 @@
+# count = 0
+# maxValue = 5
+#
+# constantValue = 1
+# entry = True
+# valid = True
+# # while count < maxValue:
+# #     print(str(count) + str(count + constantValue))
+# #     count = count + 1
+#
+# # ERROR: Inconsistent symbols and logic
+# while count <= maxValue:
+#     print("welcome to Arens crib")
+#     # Logic should allow entry UNTIL maxValue is hit
+#     # Logic to record entry
+#     if entry and valid: # BITWISE ERROR: Incorrect operator for logical AND
+#         count = count + 1
+
+number1 = 10
+number2 = 3
+
+print(number1/number2)
+print(number1//number2)
+print(number1%number2)
+print(number1**number2)
